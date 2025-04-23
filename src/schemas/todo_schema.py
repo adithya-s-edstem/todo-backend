c5e77bffdcb0e7135ee42162b5a29b82c8b16c53
Adithya S Sekhar <adithya.s@edstem.com>
2025-04-23 17:02:56 +0530

    todoschema missing completed

--- src/schemas/todo_schema.py
+++ src/schemas/todo_schema.py
@@ -3,3 +3,4 @@ from marshmallow import Schema, fields, validate
 class TodoSchema(Schema):
     title = fields.String(required=True, validate=validate.Length(min=1, error="Title should not be empty"))
     description = fields.String()
+    completed = fields.Boolean()
